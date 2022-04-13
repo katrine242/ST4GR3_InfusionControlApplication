7c5982b444d328eb7eafcd638a521bcfc792b920
katrine242 <katrinejepsen@gmail.com>
2022-04-13 14:06:43 +0200

    nuget pakker og testprojekt

---
+++ ST4GR3_test_ICA/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace ST4GR3_test_ICA
+{
+   public class Tests
+   {
+      [SetUp]
+      public void Setup()
+      {
+      }
+
+      [Test]
+      public void Test1()
+      {
+         Assert.Pass();
+      }
+   }
+}
