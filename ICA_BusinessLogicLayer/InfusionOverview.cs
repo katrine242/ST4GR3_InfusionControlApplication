8c1e004e5243a49b93edafa810058acfa499de06
NadiaAJensen <78858892+NadiaAJensen@users.noreply.github.com>
2022-05-09 22:13:16 +0200

    Metoder oprettet i klasserne til services og microsoft.entityframeworkCore er installeret

--- ICA_BusinessLogicLayer/InfusionOverview.cs
+++ ICA_BusinessLogicLayer/InfusionOverview.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DTO_Library;
 
 namespace ICA_Model
 {
@@ -15,12 +16,12 @@ namespace ICA_Model
             _infusionPlanBook = infusionPlanBook;
         }
 
-        public async Task<IEnumerable<InfusionPlan>> GetAllInfusionPlans()
+        public async Task<IEnumerable<DTO_Infusionplan>> GetAllInfusionPlans()
         {
             return await _infusionPlanBook.GetAllInfusionPlans();
         }
 
-        public async Task CreateInfusionPlan(InfusionPlan infusionPlan)
+        public async Task CreateInfusionPlan(DTO_Infusionplan infusionPlan)
         {
             await _infusionPlanBook.AddInfusionPlan(infusionPlan);
         }
