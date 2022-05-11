13407c744680288cf0a6a057b89978e573976da2
katrine242 <katrinejepsen@gmail.com>
2022-05-11 14:11:55 +0200

    oprettet view til menuwwindowview

---
+++ ST4GR3_InfusionControlApplication/Views/MenuWindowView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ST4GR3_InfusionControlApplication.Views
+{
+   /// <summary>
+   /// Interaction logic for MenuWindowView.xaml
+   /// </summary>
+   public partial class MenuWindowView : UserControl
+   {
+      public MenuWindowView()
+      {
+         InitializeComponent();
+      }
+   }
+}
