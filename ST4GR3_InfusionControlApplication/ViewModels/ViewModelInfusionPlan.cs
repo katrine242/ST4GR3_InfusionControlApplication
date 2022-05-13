81c1cf9439a56fb2be256c08fb4a7420eea7d1aa
katrine242 <katrinejepsen@gmail.com>
2022-05-13 10:40:04 +0200

    added ViewModelDataInfusion

--- ST4GR3_InfusionControlApplication/ViewModels/ViewModelInfusionPlan.cs
+++ ST4GR3_InfusionControlApplication/ViewModels/ViewModelInfusionPlan.cs
@@ -11,22 +11,8 @@ namespace ST4GR3_InfusionControlApplication.ViewModels
 {
    public class ViewModelInfusionPlan:ViewModelBase
    {
-      private readonly InfusionPlan _infusionPlan;
-      public string PatientName => _infusionPlan.InfusionData.PatientName;
-      public string CPR => _infusionPlan.InfusionData.CPR.ToString();
-      public string Height => _infusionPlan.InfusionData.Height.ToString();
-      public string Weight => _infusionPlan.InfusionData.Weight.ToString();
-      public string MedicineName => _infusionPlan.InfusionData.MedicineName;
-      public string BatchId => _infusionPlan.InfusionData.BatchId.ToString();
-      public string MachineID => _infusionPlan.InfusionData.MachineID.ToString();
-      public DateTime Fulltime => Convert.ToDateTime(_infusionPlan.InfusionData.Fulltime);
-      public DateTime TimeLeft { get; }
-      public List<DTO_TimeFlow> DtoTimeFlowList { get; set; }
+ 
 
-      public ViewModelInfusionPlan(InfusionPlan infusionPlan)
-      {
-         _infusionPlan = infusionPlan;
-      }
       public ICommand BackCommand { get; }
 
    }
