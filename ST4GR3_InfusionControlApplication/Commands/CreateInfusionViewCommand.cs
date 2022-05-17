4ecbea9ba1a75787d5d42a7b57459ced90a835ed
katrine242 <katrinejepsen@gmail.com>
2022-05-17 12:12:16 +0200

    weight blev lagt ind var dobbelt

--- ST4GR3_InfusionControlApplication/Commands/CreateInfusionViewCommand.cs
+++ ST4GR3_InfusionControlApplication/Commands/CreateInfusionViewCommand.cs
@@ -43,7 +43,6 @@ namespace ST4GR3_InfusionControlApplication.Commands
             InfusionPlan infusionPlan =
                new InfusionPlan(
                   new Medicine().GetMedicine(_infusionOverview.Configlist, _viewModelCreateInfusion.Medicine),_viewModelCreateInfusion.MachineID,_viewModelCreateInfusion.BatchID,_viewModelCreateInfusion.CPR,_viewModelCreateInfusion.Weight,_viewModelCreateInfusion.Patient);
-            infusionPlan.Weight = _viewModelCreateInfusion.Weight;
             _infusionOverview.CreateInfusionPlan(infusionPlan);
             _menuViewNavigationService.Navigate();
 
